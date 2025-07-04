e697030f7032e6daa213dbfcdf108ca771b14e00
JackBaraka <jack.baraka15894@gmail.com>
2025-07-04 05:45:58 +0300

    updated post css config

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
 
 export default {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: [tailwindcss, autoprefixer, cssnano()],
 };
-
+/*
+  If you want to add more PostCSS plugins, include them in the plugins array above.
+  Example:
+  plugins: [tailwindcss, autoprefixer, cssnano()],
+*/
 // Note: Ensure that you have the necessary PostCSS plugins installed in your project.
 // You can install them using npm or yarn:
